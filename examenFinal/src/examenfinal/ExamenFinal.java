43b3ac9a48907236519dd85a524e6022bd9a31d7
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-05-25 13:41:43 -0600

    Total cigarros

--- examenFinal/src/examenfinal/ExamenFinal.java
+++ examenFinal/src/examenfinal/ExamenFinal.java
@@ -12,10 +12,28 @@ public class ExamenFinal {
         String correo = "al071392@uacam.mx";
         System.out.println("Mi nombre es: " + nombre + "\nMi matricula es: " + matricula +
                 "\nMi correo electronico es: " + correo);
-        
     }
+    
+    //Proyecto facultad de quimica
+    //92 cantidad de hojas
+    //11 tiempo de picado
+    //07 aroma
+    //13 azucar
+    //2 nicotina
+    public static void cigarro (){
+        int cantHojas = 92;
+        int tiemPicado = 11;
+        int aroma = 07;
+        int azucar = 13;
+        int nicotina = 2;
+        int totalCigarrillos;
+        totalCigarrillos = cantHojas + tiemPicado + aroma + azucar + nicotina;
+        System.out.println(totalCigarrillos);
+    }
+    
     public static void main(String[] args) {
         author71392();
+        cigarro();
     }
     
 }
