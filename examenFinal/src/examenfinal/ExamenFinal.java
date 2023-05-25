ed0f4d2372b597e350837afeddb833873fc4a4b9
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-05-25 13:25:40 -0600

    Mis datos

---
+++ examenFinal/src/examenfinal/ExamenFinal.java
@@ -0,0 +1,21 @@
+package examenfinal;
+
+/**
+ *
+ * @author BrayanChan
+ */
+public class ExamenFinal {
+    
+    public static void author71392 (){
+        String nombre = "Brayan Nathanael Chan Pacheco";
+        int matricula = 71392;
+        String correo = "al071392@uacam.mx";
+        System.out.println("Mi nombre es: " + nombre + "\nMi matricula es: " + matricula +
+                "\nMi correo electronico es: " + correo);
+        
+    }
+    public static void main(String[] args) {
+        author71392();
+    }
+    
+}
